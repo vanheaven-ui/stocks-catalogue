71b4effc172473873e82ddd69caa62643a4ae42b
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-26 16:42:12 +0300

    Receive and display stocks data

--- src/components/StockList.js
+++ src/components/StockList.js
@@ -1,13 +1,32 @@
+import { useDispatch, useSelector } from 'react-redux';
+import useFetch from '../common/hooks/useFetch';
+import { listEndPoint } from '../constants';
+import { getStockList } from '../redux/actions';
 import Stock from './Stock';
 import StocksFilter from './StockFilter';
 
-const StockList = () => (
-  <>
-    <StocksFilter />
-    <h2>This is gonna be how the stocks are displayed on the home page</h2>
-    <Stock />
-    <p>Would be better if we can have pagination</p>
-  </>
-);
+const StockList = () => {
+  const dispatch = useDispatch();
+  const { data: stocks } = useFetch(listEndPoint);
+  dispatch(getStockList(stocks));
+  const { filter } = useSelector(state => state.filter.filter);
+  console.log(filter);
+
+  // const renderFilteredStocks = () => {
+
+  // }
+  return (
+    <>
+      <StocksFilter />
+      <h2>Stock List</h2>
+      { stocks && stocks.slice(0, 100).map(stock => (
+        <article key={`stck-${stock.symbol}`}>
+          <Stock stock={stock} />
+        </article>
+      ))}
+      <p>Would be better if we can have pagination</p>
+    </>
+  );
+};
 
 export default StockList;
