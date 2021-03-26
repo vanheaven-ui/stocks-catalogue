e11a570a99f1467e274051bcb7a334ab7579cf02
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-26 16:48:59 +0300

    Define getStocksByPrice function

---
+++ src/redux/selectors.js
@@ -0,0 +1,22 @@
+const getStocksByPrice = (stocks, priceRange) => (
+  stocks.filter(stock => {
+    switch (priceRange) {
+      case '<100':
+        return stock.price < 100;
+      case '100 < Price < 200':
+        return stock.price > 100 && stock.price < 200;
+      case '200 < Price < 300':
+        return stock.price > 200 && stock.price < 300;
+      case '300 < Price < 400':
+        return stock.price > 300 && stock.price < 400;
+      case '400 < Price < 500':
+        return stock.price > 400 && stock.price < 500;
+      case '>500':
+        return stock.price > 500;
+      default:
+        return null;
+    }
+  })
+);
+
+export default getStocksByPrice;
