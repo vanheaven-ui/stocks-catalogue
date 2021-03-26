62417e7588fe221d21328d3bc13f92ad3a057428
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-26 16:41:17 +0300

    Receive stock from redux store as props and display details

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,5 +1,27 @@
-const Stock = () => (
-  <h2>This defines the layout of each stock</h2>
+import { PropTypes } from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Stock = ({ stock }) => (
+  <>
+    <div className="company-im">
+      Co. image
+    </div>
+    <div className="stock-stats">
+      {stock.symbol}
+      <br />
+      { stock.name }
+      <br />
+      { stock.price }
+      <br />
+      { stock.exchange }
+      <br />
+    </div>
+    <Link to="/stocks/:id">Details</Link>
+  </>
 );
+
+Stock.propTypes = {
+  stock: PropTypes.instanceOf(Object).isRequired,
+};
 
 export default Stock;
