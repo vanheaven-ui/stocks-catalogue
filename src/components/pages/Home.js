5f6b47f4c8425a54cd2cc275426170fb5d70419a
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-29 13:37:43 +0300

    Impement the JSX return for Home Component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,33 @@
+import {
+  Card, Col, Container, Row,
+} from 'react-bootstrap';
+import Hero from '../Hero';
 import StockList from '../StockList';
+import heroBar from '../../images/hero-bar.png';
+import IntroAlert from '../IntroAlert';
 
 const Home = () => (
   <>
-    <h2>This is a Home page</h2>
+    <section className="hero">
+      <Container fluid>
+        <Row style={{ height: 362 }}>
+          <Col sm={12} lg={7} style={{ height: 'inherit' }}>
+            <Hero />
+          </Col>
+          <Col sm={12} lg={5} style={{ height: 'inherit' }}>
+            <Card border="none" className="bg-dark text-light" style={{ height: 'inherit', verticleAlign: 'middle' }}>
+              <Card.Img src={heroBar} alt="Card image" style={{ width: 300, margin: '0 auto' }} />
+              <Card.ImgOverlay>
+                <Card.Title>
+                  <IntroAlert />
+                </Card.Title>
+              </Card.ImgOverlay>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+
+    </section>
     <StockList />
   </>
 );
