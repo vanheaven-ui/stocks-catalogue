f6aa87b905ecb22f55d69f5ac3762b5190c49298
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-29 13:29:52 +0300

    Add inline styles and link to news page

--- src/common/components/Navbar.js
+++ src/common/components/Navbar.js
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <nav className="navbar">
+  <nav className="navbar" style={{ color: '#007bff' }}>
     <h2><Link to="/">StocksCatalogue</Link></h2>
     <div className="links">
-      <Link to="/">Home</Link>
+      <Link to="/" style={{ margin: '0 10px 0 0' }}>Home</Link>
+      <Link to="/news">Stock News</Link>
     </div>
   </nav>
 );
