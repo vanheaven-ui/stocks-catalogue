ec2da942bfff514795e706eae9723a828736ecb2
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-30 17:39:02 +0300

    Update the Navbar style rules and layout

--- src/common/components/Navbar.js
+++ src/common/components/Navbar.js
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom';
+import logo from '../../images/logo.jpeg';
 
 const Navbar = () => (
-  <nav className="navbar" style={{ color: '#007bff' }}>
-    <h2><Link to="/">StocksCatalogue</Link></h2>
+  <nav className="navbar sticky-top mb-2" style={{ color: '#007bff' }}>
+    <h2>
+      <Link to="/">
+        <img src={logo} alt="my logo" style={{ width: 60, height: 60, marginRight: 5 }} />
+        StocksCatalogue
+      </Link>
+    </h2>
     <div className="links">
       <Link to="/" style={{ margin: '0 10px 0 0' }}>Home</Link>
-      <Link to="/news">Stock News</Link>
+      <Link to="/news">StockNews</Link>
     </div>
   </nav>
 );
