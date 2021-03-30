166e1a97e798636b32f28822d8add4b8c66c7409
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-30 17:46:44 +0300

    Modify the news page layout and styler rules

--- src/components/pages/News.js
+++ src/components/pages/News.js
@@ -1,4 +1,4 @@
-import { Badge } from 'react-bootstrap';
+import { Badge, Container } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { getNews } from '../../redux/actions';
 
@@ -12,31 +12,39 @@ const News = () => {
   dispatch(getNews(news));
   return (
     <section className="news">
-      {news && news.map(item => (
-        <article className="news-article" key={item.symbol}>
-          <header>
-            { item.title && (
-              <h2>
-                {item.title}
-                <Badge variant="danger">{item.symbol}</Badge>
-              </h2>
-            )}
-            <div className="time-author">
-              {item.publishedDate && <small>{item.publishedDate}</small>}
-              <div className="author">
-                {item.site && <h4>{item.site}</h4>}
-                {item.url && <a href={item.url}>Website</a>}
+      <Container style={{ backgroundColor: '#fff', textAlign: 'center' }}>
+        {news && news.map(item => (
+          <article className="news-article" key={item.symbol}>
+            <header>
+              { item.title && (
+                <h2>
+                  {item.title}
+                  <Badge variant="danger">{item.symbol}</Badge>
+                </h2>
+              )}
+              <div className="time-author">
+                {item.publishedDate && <small>{item.publishedDate}</small>}
+                <div className="author">
+                  {item.site && (
+                  <h4>
+                    By:
+                    {' '}
+                    {item.site}
+                  </h4>
+                  )}
+                  {item.url && <a href={item.url}>Source Url</a>}
+                </div>
+                <div className="news-img">
+                  {item.image && <img src={item.image} alt="news-hero" className="w-100" /> }
+                </div>
+                <div className="body">
+                  {item.text && <p>{item.text}</p>}
+                </div>
               </div>
-              <div className="news-img">
-                {item.image && <img src={item.image} alt="news-hero" /> }
-              </div>
-              <div className="body">
-                {item.text && <p>{item.text}</p>}
-              </div>
-            </div>
-          </header>
-        </article>
-      ))}
+            </header>
+          </article>
+        ))}
+      </Container>
     </section>
   );
 };
