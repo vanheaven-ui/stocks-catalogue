bb41bbe45bae5eb5239d03ea78250739e7914f24
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-01 16:36:25 +0300

    Use useParams hook to grab symbol from route parameters

--- src/components/pages/Details.js
+++ src/components/pages/Details.js
@@ -1,9 +1,13 @@
+import { useParams } from 'react-router-dom';
 import StockDetails from '../../containers/StockDetails';
 
-const Details = () => (
-  <>
-    <StockDetails />
-  </>
-);
+const Details = () => {
+  const { symbol } = useParams();
+  return (
+    <>
+      <StockDetails symbol={symbol} />
+    </>
+  );
+};
 
 export default Details;
