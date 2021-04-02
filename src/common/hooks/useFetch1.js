68aedb6875c84a78c61de9d696da55a5e10490d5
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-02 16:44:07 +0300

    Add abort case error handler

--- src/common/hooks/useFetch1.js
+++ src/common/hooks/useFetch1.js
@@ -4,36 +4,50 @@ const useFetch1 = url => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
+  // useEffect(() => {
+  //   const abortCont = new AbortController();
+  //   setIsLoading(true);
+  //   const fetchData = (async () => {
+  //     const response = await fetch(url, {
+  //       signal: abortCont.signal,
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         Accept: 'application/json',
+  //       },
+  //     });
+  //     if (!response.ok) {
+  //       throw Error(`Encountered error: ${response.status}`);
+  //     }
+  //     const stockss = await response.json();
+  //     return stockss;
+  //   })();
+
+  //   fetchData.then(stocks => {
+  //     setIsLoading(false);
+  //     setData(stocks);
+  //   });
+
+  //   fetchData.catch(err => {
+  //     const alert = document.createElement('div');
+  //     alert.innerHTML = err.message;
+  //     document.body.insertAdjacentElement('afterbegin', alert);
+
+  //     setTimeout(() => alert.remove(), 1000);
+  //   });
+
+  //   return () => abortCont.abort();
+  // }, [url]);
+
   useEffect(() => {
     const abortCont = new AbortController();
     setIsLoading(true);
-    const fetchData = (async () => {
-      const response = await fetch(url, {
-        signal: abortCont.signal,
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        },
-      });
-      if (!response.ok) {
-        throw Error(`Encountered error: ${response.status}`);
-      }
-      const stockss = await response.json();
-      return stockss;
-    })();
-
-    fetchData.then(stocks => {
-      setIsLoading(false);
-      setData(stocks);
-    });
-
-    fetchData.catch(err => {
-      const alert = document.createElement('div');
-      alert.innerHTML = err.message;
-      document.body.insertAdjacentElement('afterbegin', alert);
-
-      setTimeout(() => alert.remove(), 1000);
-    });
+    fetch(url, { signal: abortCont.signal })
+      .then(res => res.json())
+      .then(data => {
+        setData(data);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err));
 
     return () => abortCont.abort();
   }, [url]);
