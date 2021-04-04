5d74d870b972b183d533622c82b6f30b3b820738
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-04 11:10:48 +0300

    Remove redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,6 @@
-import { compose, createStore } from 'redux';
+import { createStore } from 'redux';
 import rootReducer from './reducers';
 
-const storeEnhancers = compose(
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // eslint-disable-line
-);
-
-const store = createStore(rootReducer, storeEnhancers);
+const store = createStore(rootReducer);
 
 export default store;
