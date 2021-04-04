77b8235a3a334f70aa56fa961da2ab8e4c8bdbdf
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-04 10:58:20 +0300

    Remove commended codelines and implement alert for error handling

--- src/common/hooks/useFetch1.js
+++ src/common/hooks/useFetch1.js
@@ -4,40 +4,6 @@ const useFetch1 = url => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  // useEffect(() => {
-  //   const abortCont = new AbortController();
-  //   setIsLoading(true);
-  //   const fetchData = (async () => {
-  //     const response = await fetch(url, {
-  //       signal: abortCont.signal,
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //         Accept: 'application/json',
-  //       },
-  //     });
-  //     if (!response.ok) {
-  //       throw Error(`Encountered error: ${response.status}`);
-  //     }
-  //     const stockss = await response.json();
-  //     return stockss;
-  //   })();
-
-  //   fetchData.then(stocks => {
-  //     setIsLoading(false);
-  //     setData(stocks);
-  //   });
-
-  //   fetchData.catch(err => {
-  //     const alert = document.createElement('div');
-  //     alert.innerHTML = err.message;
-  //     document.body.insertAdjacentElement('afterbegin', alert);
-
-  //     setTimeout(() => alert.remove(), 1000);
-  //   });
-
-  //   return () => abortCont.abort();
-  // }, [url]);
-
   useEffect(() => {
     const abortCont = new AbortController();
     setIsLoading(true);
@@ -50,18 +16,16 @@ const useFetch1 = url => {
     })
       .then(res => res.json())
       .then(data => {
-        console.log(data);
         setData(data);
         setIsLoading(false);
       })
       .catch(err => {
-        console.log(err.message);
-        // const alert = document.createElement('div');
-        // alert.innerHTML = err.message;
-        // alert.classList.add('my-alert');
-        // document.body.insertAdjacentElement('afterbegin', alert);
+        const alert = document.createElement('div');
+        alert.innerHTML = err.message;
+        alert.classList.add('my-alert');
+        document.body.insertAdjacentElement('afterbegin', alert);
 
-        // setTimeout(() => alert.remove(), 1000);
+        setTimeout(() => alert.remove(), 1000);
       });
     return () => abortCont.abort();
   }, [url]);
