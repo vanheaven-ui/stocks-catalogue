6ca4ca6c8d08856e6fcae322687eee38901b26dc
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-04 13:09:02 +0300

    Resolve eslint offenses

--- src/containers/StockList.js
+++ src/containers/StockList.js
@@ -27,7 +27,7 @@ const StockList = () => {
   }
 
   const stocks = useSelector(state => state.stocks.stocks);
-  
+
   const filter = useSelector(state => state.filter.filter);
 
   const exchanges = stocks ? stocks.slice(0, 1000).map(stock => stock.exchange) : null;
