6f1cbc20b888f9719948aba683c89b374217dc05
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-04 07:03:08 +0300

    Modify tests

--- src/__Tests__/components/pages/Details.test.js
+++ src/__Tests__/components/pages/Details.test.js
@@ -1,23 +1,29 @@
 import ReactDOM from 'react-dom';
+
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Details from '../../../components/pages/Details';
 import store from '../../../redux/store';
 import rootNode from '../../helpers';
 
 describe('Details page', () => {
   test('renders without crashing', () => {
-    ReactDOM.render(<Provider store={store}><Details /></Provider>, rootNode());
+    ReactDOM.render(
+      <Router><Provider store={store}><Details /></Provider></Router>, rootNode(),
+    );
   });
 
   test('doesnot change unexpectedly', () => {
-    const DetailsTree = renderer.create(<Provider store={store}><Details /></Provider>).toJSON();
+    const DetailsTree = renderer.create(
+      <Router><Provider store={store}><Details /></Provider></Router>,
+    ).toJSON();
     expect(DetailsTree).toMatchSnapshot();
   });
 
   test('renders the right wrapper element', () => {
-    const { container } = render(<Provider store={store}><Details /></Provider>);
+    const { container } = render(<Router><Provider store={store}><Details /></Provider></Router>);
     expect(container.firstChild.classList.contains('stockDetails')).toBe(true);
   });
 });
